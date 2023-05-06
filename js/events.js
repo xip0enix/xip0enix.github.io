e2d8af2314f621959774b79683bd8580eaabe39f
xip0enix <108542693+xip0enix@users.noreply.github.com>
2023-05-06 08:05:55 +0200

    added loading screen

--- js/events.js
+++ js/events.js
@@ -4,30 +4,38 @@ const ort = 'Online';
 
 // Function to fetch and display the events
 function getMINT(){
-  fetch(api)
-  .then(response => response.json())
-  .then(data => {
-    // Get the events table body element
-    const eventsTableBody = document.getElementById('events-table-body');
+  // Zeige den Ladebildschirm an
+  const loadingScreen = document.getElementById('loading-screen');
+  // Show the loading screen
+  loadingScreen.style.display = 'block';
 
-    // Loop through each event in the data
-    for (let i = 0; i < data.length; i++) {
-      // If the event accommodation includes the search term, create a new table row and add the event data to it
-      if (data[i].eventAccommodation.includes(ort)) {
-        const eventRow = document.createElement('tr');
-        const eventName = document.createElement('td');
-        eventName.textContent = data[i].title;
-        const eventDateRange = document.createElement('td');
-        eventDateRange.textContent = data[i].eventRange;
-        const eventMonth = document.createElement('td');
-        eventMonth.textContent = data[i].eventMonth;
-        eventRow.appendChild(eventName);
-        eventRow.appendChild(eventDateRange);
-        eventRow.appendChild(eventMonth);
-        eventsTableBody.appendChild(eventRow);
+  fetch(api)
+    .then(response => response.json())
+    .then(data => {
+      // Hide the loading screen
+      loadingScreen.style.display = 'none';
+
+      // Get the events table body element
+      const eventsTableBody = document.getElementById('events-table-body');
+
+      // Loop through each event in the data
+      for (let i = 0; i < data.length; i++) {
+        // If the event accommodation includes the search term, create a new table row and add the event data to it
+        if (data[i].eventAccommodation.includes(ort)) {
+          const eventRow = document.createElement('tr');
+          const eventName = document.createElement('td');
+          eventName.textContent = data[i].title;
+          const eventDateRange = document.createElement('td');
+          eventDateRange.textContent = data[i].eventRange;
+          const eventMonth = document.createElement('td');
+          eventMonth.textContent = data[i].eventMonth;
+          eventRow.appendChild(eventName);
+          eventRow.appendChild(eventDateRange);
+          eventRow.appendChild(eventMonth);
+          eventsTableBody.appendChild(eventRow);
+        }
       }
-    }
-  })
+    });
 }
 
 // Call the function to display the events
