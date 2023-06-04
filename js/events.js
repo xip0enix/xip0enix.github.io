1d7c6703a452115cd7eba32538c1b8953f5af634
xip0enix <108542693+xip0enix@users.noreply.github.com>
2023-06-04 16:52:44 +0200

    Update events.js

--- js/events.js
+++ js/events.js
@@ -3,7 +3,7 @@ const api = 'https://mint-api.onrender.com/api/veranstaltungen';
 const ort = 'Online';
 
 // Function to fetch and display the events
-function getMINT(){
+function getMINT() {
   // Zeige den Ladebildschirm an
   const loadingScreen = document.getElementById('loading-screen');
   // Show the loading screen
@@ -22,9 +22,25 @@ function getMINT(){
       for (let i = 0; i < data.length; i++) {
         // If the event accommodation includes the search term, create a new table row and add the event data to it
         if (data[i].eventAccommodation.includes(ort)) {
+          const eventURL = data[i].url;
           const eventRow = document.createElement('tr');
           const eventName = document.createElement('td');
           eventName.textContent = data[i].title;
+
+          // Add event listener to redirect to the event URL when hovering over the event name
+          eventName.addEventListener('mouseenter', () => {
+            eventName.style.cursor = 'pointer';
+            eventName.style.color = '#00ff6e'; // Change the color to red when hovering
+          });
+
+          eventName.addEventListener('mouseleave', () => {
+            eventName.style.color = ''; // Reset the color when not hovering
+          });
+
+          eventName.addEventListener('click', () => {
+            window.open(eventURL, '_blank');
+          });
+
           const eventDateRange = document.createElement('td');
           eventDateRange.textContent = data[i].eventRange;
           const eventMonth = document.createElement('td');
